29e3ec27a97947a14ee85d7418da6b888e561933
Mike <miguel01amd@gmail.com>
2021-03-24 17:31:40 -0600

    Modulo de Content Types

--- structureContent/templates/block-nodes.tpl.php
+++ structureContent/templates/block-nodes.tpl.php
@@ -1,20 +1,17 @@
-<section class="banner">
-  <div class="row">
-    <div class="col-12">
-      <div class="row justify-content-center">
-        <?php if( isset($content['title']) && !empty($content['title']) ): ?>
-          <h1><?php print $content['title']; ?></h1>
-        <?php endif; ?>
+<?php if( isset($content) && !empty($content) ): ?>
+  <section class="banner">
+    <div class="row">
+      <div class="col-12">
+        <div class="row justify-content-center">
+          <?php foreach($content as $nid => $field): ?>
+            <div class="nota">
+              <h3><?php print $field['title']; ?></h3>
+              <p><?php print $field['desc']; ?></p>
+              <img src="<?php print $field['img']; ?>" alt="$field['title']; ?>">              
+            </div>
+          <?php endforeach; ?>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="row">
-    <div class="col-12">
-      <div class="row justify-content-center">
-        <?php if( isset($content['body']) && !empty($content['body']) ): ?>
-          <p><?php print $content['body']; ?></p>
-        <?php endif; ?>
-      </div>
-    </div>
-  </div>
-</section>
+  </section>
+<?php endif; ?>
